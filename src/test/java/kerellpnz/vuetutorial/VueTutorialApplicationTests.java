077c6b026dd70835d6715a5716690d42417d3505
k.noskov <k.noskov@lancelot.prod>
2022-04-29 21:33:16 +0300

    init

---
+++ src/test/java/kerellpnz/vuetutorial/VueTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package kerellpnz.vuetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
