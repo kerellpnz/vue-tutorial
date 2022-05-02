6f603db5112e379180e1ef25cab42c047c06d496
kerell <kerellpnz@gmail.com>
2022-05-02 21:49:52 +0300

    In work, part II

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,80 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h1>Posts page</h1>
+    <div class="app_btns">
+      <my-button
+          @click="showDialog"
+          style="margin: 15px 0px"
+      >
+        Create post
+      </my-button>
+      <my-select
+
+      ></my-select>
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
     <post-list
         :posts="posts"
         @remove="removePost"
+        v-if="!isPostsLoading"
     />
-
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
     import PostForm from "@/components/PostForm";
     import PostList from "@/components/PostList";
+    import MyDialog from "@/components/UI/MyDialog";
+    import MyButton from "@/components/UI/MyButton";
+    import axios from 'axios';
+    import MySelect from "@/components/UI/MySelect";
+
     export default {
       components: {
+        MySelect,
+        MyButton,
+        MyDialog,
         PostList, PostForm
       },
       data() {
-          return {
-              posts: [
-                {id: 1, title: 'JS 1', body: 'Description of post 1'},
-                {id: 2, title: 'JS 2', body: 'Description of post 2'},
-                {id: 3, title: 'JS 3', body: 'Description of post 3'},
-                {id: 4, title: 'JS 4', body: 'Description of post 4'},
-              ],
-          }
+        return {
+          posts: [],
+          dialogVisible: false,
+          isPostsLoading: false
+        }
       },
       methods: {
         // createPost(post) -> (post) - типа return из одноим. метода в PostForm.vue
         // далее внутри {} описываю любые манипалюции с полученным post
         createPost(post) {
           this.posts.push(post)
+          this.dialogVisible = false;
         },
         removePost(post) {
           this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog() {
+          this.dialogVisible = true;
+        },
+        async fetchPosts() {
+          try {
+            this.isPostsLoading = true
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            this.posts = response.data
+          } catch (e) {
+            alert('Error')
+          } finally {
+            this.isPostsLoading = false
+          }
         }
+      },
+      mounted() {
+        this.fetchPosts()
       }
     }
 </script>
@@ -50,6 +88,11 @@
 
 .app {
   padding: 20px;
+}
+
+.app_btns {
+  display: flex;
+  justify-content: space-between;
 }
 
 </style>
--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div class="dialog" v-if="show === true" @click.stop="hideDialog">
+      <div @click.stop class="dialog__content">
+        <slot></slot>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -7,12 +11,35 @@ export default {
   name: 'my-dialog',
   props: {
     show: {
-      // 1:10
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    hideDialog() {
+      this.$emit('update:show', false)
     }
   }
 }
 </script>
 
 <style scoped>
+.dialog {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0,0,0, 0.5);
+  position: fixed;
+  display: flex;
+}
 
+.dialog__content {
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
+}
 </style>
